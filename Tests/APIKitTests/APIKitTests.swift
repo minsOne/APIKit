1de84a9cae3ecd8799c87985116d6558cf40e03e
minsOne <jungminahn.minsone@gmail.com>
2023-04-13 23:57:57 +0900

    update test

--- Tests/APIKitTests/APIKitTests.swift
+++ Tests/APIKitTests/APIKitTests.swift
@@ -9,4 +9,12 @@ final class APIKitTests: XCTestCase {
         let result2 = await HTTPBin.Post(parameter: .init(name: "jungmin", age: "35")).request()
         print(result2)
     }
+
+    func testURLInfo() {
+        let info = API.URLInfo(host: "httpbin.org", port: 80, path: "/get", query: ["hello":"world"])
+        let url = info.url.absoluteString
+        print(url)
+        XCTAssertEqual(url,
+                       "https://httpbin.org:80/get?hello=world")
+    }
 }
